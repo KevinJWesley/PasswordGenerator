4dfc19bcfe9cfb269c1377cd2e77b992fe3e0f01
kevinjwesley <kevin.john.wesley@gmail.com>
2021-04-23 19:39:05 -0600

    added objects

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,23 @@
 // Assignment Code
 
-//when i click the button i am presented with a series of prompts
+var characters = {
+  lower: "abcdefghijklmnop",
+  upper: "abcdefghijklmnop",
+  num: "0123456789",
+  special: "!@#$%^",
+};
 
 function userPrompt() {
   var characterAmount = prompt(
     "How many characters would you like in your password?"
   );
+
   if (characterAmount < 8 || characterAmount > 128) {
     alert("Please Choose a length of password between 8 and 128 characters.");
   } else {
-    var specialCharacters = confirm(
-      "Would You Like to Add Lowerscase Letters?"
-    );
-    var specialCharacters = confirm("Would you like to Add Uppercase Letters?");
-    var specialCharacters = confirm("Woud You Like to Add Numbers?");
+    var lowercase = confirm("Would You Like to Add Lowerscase Letters?");
+    var uppercase = confirm("Would you like to Add Uppercase Letters?");
+    var numbers = confirm("Woud You Like to Add Numbers?");
     var specialCharacters = confirm(
       "Would You Like Your Password To Contain Special Characters?"
     );
@@ -25,10 +29,11 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", characterPrompt);
+generateBtn.addEventListener("click", userPrompt);
